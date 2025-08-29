599b59c85828c6ca25357f7d6a9787214a010f7c
Silas <chibukesilas@gmail.com>
2025-08-29 01:10:07 +0100

    First Commit

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -3,22 +3,19 @@ import ProfileInfo from "@/components/profilePage/profileInfo";
 import PostsGallery from "@/components/profilePage/PostsGallery";
 import { posts } from "@/lib/data/postsData";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ username: string }>;
-}) {
-  const username = (await params).username;
+export default function Page({ params }: { params: { username: string } }) {
+  const username = params.username;
   const post = posts.find((post) => post.user.name === username);
   const user = post?.user;
+
   return (
     <div className="min-h-screen">
       <ProfileInfo
         username={username}
-        img={user?.avatar}
-        bio={post?.bio}
-        followers={user?.followers}
-        followings={user?.following}
+        img={user?.avatar ?? ""} // fallback to empty string
+        bio={post?.bio ?? ""}
+        followers={user?.followers ?? 0}
+        followings={user?.following ?? 0}
       />
       <PostsGallery />
     </div>
